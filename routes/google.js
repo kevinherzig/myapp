7b2e0a506361c4681132da77ff3f61d78abb9ff0
Kevin Herzig <kevin@herzig.net>
2022-04-13 22:43:11 -0400

    kafka write to server is working

--- routes/google.js
+++ routes/google.js
@@ -5,32 +5,31 @@ const { Kafka } = require('kafkajs')
 
 const kafka = new Kafka({
     clientId: 'my-app',
-    brokers: ['kafka1:9092', 'kafka2:9092']
+    brokers: ['localhost:9092']
 })
 
 
 const producer = kafka.producer()
-const consumer = kafka.consumer({ groupId: 'test-group' })
-
 var router = express.Router();
 
-// To prevent connection errors until we have a kafka server running
+// To prevent connection errors until we have a kafka server running, set this to FALSE
 const use_kafka = false;
 
-/* GET users listing. */
+if (use_kafka)
+    producer.connect();
+
+/* GET search results */
 router.get('/', function (req, res, next) {
+    if (use_kafka)
+        producer.send({
+            topic: 'first_kafka_topic',
+            messages: [
+                { value: req.query['search'] },
+            ],
+        });
+
 
     googleIt({ 'query': req.query['search'] }).then(results => {
-        // ** write query['search'] to kafka topic here **
-        if (use_kafka)
-            producer.send({
-                topic: 'test-topic',
-                messages: [
-                    { value: query['search'] },
-                ],
-            })
-
-
         res.send(results);
     }).catch(e => {
         // any possible errors that might have occurred (like no Internet connection)
